862c51bf7e32e91aca488fe2b7785d11193e9996
Peto626 <empg014@ciencias.unam.mx>
2022-11-06 20:19:55 -0600

    Algoritmo de gradiente descendente

---
+++ Gradiente/Readme.md
@@ -0,0 +1,6 @@
+
+# Algoritmo del gradiente descendente
+
+Para ejecutar: python3 gradDesc.py
+
+Se busca minimizar la función f(x,y) = x²+y² empezando en un punto aleatorio entre -500 y 500.
---
+++ Gradiente/gradDesc.py
@@ -0,0 +1,55 @@
+
+
+import random
+import sympy as sym
+from pybigparser import evaluator
+
+## construye el vector gradiente.
+# @param f función simbólica (construida con sympy).
+# @param simbolos tupla de símbolos (variables de la función).
+def construyeGradiente(f, simbolos):
+    gradSym = []
+    for i in range(len(simbolos)):
+        gradSym.append(sym.diff(f, simbolos[i]))
+    print("Vector gradiente:", gradSym)
+    gradFun = []
+    for expr in gradSym:
+        gradFun.append(str(expr))
+    return gradFun
+
+## Algoritmo de gradiente descendente
+# gradiente: Vector gradiente como una lista de strings
+# variables: Variables de la función como una lista de strings
+# alfa: valor de la constante alfa
+# iteraciones: número de iteraciones a realizar en el algoritmo
+def gradienteDescendente(gradiente, variables, alfa, iteraciones):
+    vector = []
+    grad2 = []
+    for elem in gradiente:
+        parser = evaluator.MathParser()
+        parser.set_function(elem)
+        grad2.append(parser)
+    for i in range(len(variables)):
+        vector.append(random.randint(-500, 500))
+    for i in range(iteraciones):
+        vectorNuevo = []
+        for subFuncion in grad2:
+            for j in range(len(variables)):
+                subFuncion.add_sub(variables[j], vector[j])
+            subFuncion.evaluate()
+            vectorNuevo.append(subFuncion.value)
+        for j in range(len(vectorNuevo)):
+            vector[j] = vector[j] - alfa*vectorNuevo[j]
+    return vector
+
+
+variables = 'x y'
+tupla = sym.symbols(variables)
+funcion = tupla[0]**2+tupla[1]**2 # esta es la función f(x,y)=x²+y²
+funcionLambda = sym.lambdify(tupla, funcion)
+print("Función: f(x,y) =", funcion)
+gradiente = construyeGradiente(funcion, tupla)
+mejorVector = gradienteDescendente(gradiente, variables.split(" "), 0.001, 10000)
+print("Mejor vector encontrado: (x, y):", mejorVector)
+print("f(x, y) =", funcionLambda(mejorVector[0], mejorVector[1]))
+
